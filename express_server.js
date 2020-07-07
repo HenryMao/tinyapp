e5b9427ef9b311356b113953d89bdfae98d347cb
HenryMao <henry60308@live.cn>
2020-07-07 01:29:39 +0000

    missing the redirect before last step

--- express_server.js
+++ express_server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  let tempUrl = generateRandString();
+  urlDatabase[tempUrl] = req.body.longURL;
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,3 +54,12 @@ app.get("/urls/:shortURL", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+const generateRandString = function(){
+  let result = '';
+  const alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for(let i = 0; i < 6; i++){
+    result += alph.charAt(Math.floor(Math.random() * alph.length));
+  }
+}
